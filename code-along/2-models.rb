8310781543fcda8718dc2e31537c685fcf40278b
Yuxin Zhang <Yuxinzhang2024@u.northwestern.edu>
2024-05-02 22:08:50 +0000

    p

--- code-along/2-models.rb
+++ code-along/2-models.rb
@@ -25,12 +25,44 @@ p new_company
 new_company.save
 
 puts "There are #{Company.all.count} companies"
-# 3. query companies table to find all row with California company
 
+new_company = Company.new
+new_company["name"] = "Amazon"
+new_company["city"] = "Seattle"
+new_company["state"] = "WA"
+p new_company
+new_company.save
+
+puts "There are #{Company.all.count} companies"
+
+new_company = Company.new
+new_company["name"] = "Twitter"
+new_company["city"] = "San Francisco"
+new_company["state"] = "CA"
+p new_company
+new_company.save
+
+puts "There are #{Company.all.count} companies"
+# 3. query companies table to find all row with California company
+all_companies = Company.all
+p all_companies
 # 4. query companies table to find single row for Apple
+cali_companies = Company.where({"state" => "CA"})
+p cali_companies
 
 # 5. read a row's column value
-
+apple = Company.find_by({"name" => "Apple"})
+p apple["name"]
 # 6. update a row's column value
+amazon = Company.find_by({"name" => "Amazon"})
+amazon["url"] = "http://amazon.com"
+amazon.save
+
+x = Company.find_by({"name" => "Twitter"})
+x["name"] = "x"
+x.save
 
 # 7. delete a row
+x = Company.find_by({"name" => "x"})
+x.destroy
+puts "There are #{Company.all.count} companies"
